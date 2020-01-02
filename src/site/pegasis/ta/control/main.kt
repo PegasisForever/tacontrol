474f28ea8a66e740a3a0c149dac6b0c4f3284369
Pegasis <admin@pegasis.site>
2020-01-02 12:27:26 -0500

    Init commit

---
+++ src/site/pegasis/ta/control/main.kt
@@ -0,0 +1,35 @@
+package site.pegasis.ta.control
+
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request.Builder
+import okhttp3.RequestBody
+import org.json.simple.JSONArray
+import java.io.IOException
+
+fun main(a: Array<String>) {
+    val args = JSONArray()
+    args.addAll(a)
+    var target = "http://localhost:5006/"
+    if (args.size >= 2 && (args.first() == "-t" || args.first() == "--target")) {
+        target = args[1].toString()
+        args.removeAt(0)
+        args.removeAt(0)
+    }
+    if (args.size == 0) {
+        args.add("-h")
+    }
+
+    val request = Builder()
+        .url(target)
+        .post(args)
+        .build()
+
+    val res = OkHttpClient().newCall(request).execute()
+    if (!res.isSuccessful) throw IOException("Unexpected code $res")
+    println(res.body().string())
+}
+
+fun Builder.post(jsonArray: JSONArray): Builder {
+    return this.post(RequestBody.create(MediaType.parse("application/json"), jsonArray.toJSONString()))
+}
